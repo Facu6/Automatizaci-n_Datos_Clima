82c1b3d92219d7626b39f6f387f8c15f319876af
Facu6 <dispenzafacu6@gmail.com>
2025-04-02 15:47:35 -0300

    Se agrega archivo etl_runner y y se modifica dag para solución del problema de serialización

--- dags/dag.py
+++ dags/dag.py
@@ -1,51 +1,27 @@
 from airflow import DAG
-from airflow.decorators import task
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-import subprocess
 
-
-# Configuración del DAG
 default_args = {
     'owner' : 'usuario',
     'depends_on_past' : False,
-    'start_date' : datetime(2025, 3, 11),
+    'start_date' : datetime(2025, 4, 2),
     'email_on_failure' : False,
     'email_on_retry' : False,
     'retries' : 1,
-    'retry_delay' : timedelta(minutes=1)
+    'retries_delay' : timedelta(minutes=1)
 }
 
-dag = DAG(
-    'ETL-Diario',
+with DAG(
+    dag_id = 'ETL-Diario',
     default_args = default_args,
-    description = 'DAG para ejecutar ETL en 3 fases: Extracción, Procesamiento, Carga.',
+    description = 'ETL diario ejecutado desde runner externo',
     schedule_interval = '59 23 * * *',
-    catchup = False
-)
-
-# Definir tareas del ETL en Airflow
-@task(dag=dag)
-def extraer_datos():
-    subprocess.run(['python3', '/opt/airflow/dags/script.py', 'extraer'], check = True)
-    print('Extracción completada.')
-
-@task(dag=dag)
-def procesar_datos():
-    subprocess.run(['python3', '/opt/airflow/dags/script.py', 'procesar'], check = True)
-    print('Procesamiento completado.')
-
-@task(dag=dag)
-def cargar_datos():
-    subprocess.run(['python3', '/opt/airflow/dags/script.py', 'cargar'], check = True)
-    print('Carga completada.')
+    catchup = False,
+    tags = ['etl', 'pyspark']
+) as dag:
     
-
-# Definir flujo de ejecución
-extraer = extraer_datos()
-procesar = procesar_datos()
-cargar = cargar_datos()
-
-extraer >> procesar >> cargar
-
-
-# INSTALAR TODAS LAS DEPENDENCIAS DENTRO DEL CONTENEDOR 
+    ejecutar_etl = BashOperator(
+        task_id = 'ejecutar_etl',
+        bash_command = 'python /opt/airflow/dags/etl_runner.py'
+    )
